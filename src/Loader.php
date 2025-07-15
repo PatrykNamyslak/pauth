803636112ec543d15e5bd53485c3c490226971bc
Patryk <patryk@frontendfoundry.dev>
2025-07-15 20:50:51 +0100

    Update Loader.php

--- src/Loader.php
+++ src/Loader.php
@@ -22,19 +22,19 @@ class Loader{
 
         $requirements_met = TRUE;
         foreach ($required_db_variables as $var){
-            if (!isset($_ENV[$var]) and !empty($_ENV        [$var])){
+            if (!isset($_ENV[$var]) and !empty($_ENV[$var])){
                 $requirements_met = FALSE;
             }
         }
         if ($requirements_met){
-            $predefined_db_connection = new Database(
+            $predefined_db_connection = new Patbase(
                 host: $_ENV['PHP_AUTH_DB_HOST'],
                 database_name: $_ENV['PHP_AUTH_DB_NAME'],
                 username: $_ENV['PHP_AUTH_DB_USERNAME'],
                 password: $_ENV['PHP_AUTH_DB_PASSWORD']
             );
         }else{
-            throw new Exception("Looks like you         have not setup your configuration! Go to " .        __DIR__ . '/.env to set it up');
+            throw new \Exception("Looks like you have not setup your configuration! Go to " . __DIR__ . '/.env to set it up');
         }
     }
 }
