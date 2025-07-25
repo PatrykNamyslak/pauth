ce2fdd8a71b352fd7523eeea4ef8a4eeb7a736c2
Patryk <patryk@frontendfoundry.dev>
2025-07-26 00:03:14 +0100

    Lil fix

--- src/environment_variables.php
+++ src/environment_variables.php
@@ -1,8 +1,5 @@
 <?php
 use PatrykNamyslak\Patbase as Database;
 require_once 'functions.php';
-require_once 'autoload.php';
-// $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-// $dotenv->load();
 
 ?>
